6ee599d5469fc35dd5e1f4c1f615e8b9f66d01fb
njtsb1 <njtsb1@gmail.com>
2026-08-11 16:27:33 -0300

    Implementation of the REST API for city queries, including entities, repositories, and resources for handling geographic data.

---
+++ src/main/java/com/github/njtsb1/staties/repositories/StateRepository.java
@@ -0,0 +1,8 @@
+package com.github.njtsb1.staties.repositories;
+
+import com.github.njtsb1.staties.entities.State;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
